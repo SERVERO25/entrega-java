08f89e7b227ee6039be7eb03cce6e93772dff3a6
juanesgt16 <280826628+juanesgt16@users.noreply.github.com>
2026-05-10 16:48:33 -0500

    Update Ciclo_for_numeros.java

--- segundo-informe/Ciclo_for_numeros.java
+++ segundo-informe/Ciclo_for_numeros.java
@@ -5,11 +5,9 @@ public class Ciclo_for_numeros {
 	public static void main(String[] args) {
 		Scanner Scanner = new Scanner(System.in);
 		
-		//=========== Ciclo del 0 al 100 ===============
-		//----------- datos de entrada ---------------
 		System.out.println("Ingrese un número entre el 1 y el 100");
 		int num = Scanner.nextInt();
-		//----------- Ciclo for --------------
+		
 		for (int i = 0; i <= num; i++) {
 			System.out.print("contando hasta llegar a " + num + ": " + i);
 			if(i % 2 == 0) {
